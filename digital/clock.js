64686877f3694c21441ddcd4b15737f8929f3b79
shun-itsuki <it_shun_1125@yahoo.co.jp>
2019-08-14 15:23:57 +0900

    clock

---
+++ digital/clock.js
@@ -0,0 +1,13 @@
+// メイン関数こちらをトリガーに発火
+function clock(){
+  // １週間分の各文字列の配列
+  var weeks = new Array("Sun","Mon","Thu","Wed","Thr","Fri","Sat");
+  // 現在日時を表すインスタンスを取得
+  var now = new Date();
+  // 年
+  var y = now.getFullYear();
+  // 月0〜11で取得するため、+1する処理を記載
+  var mo = now.getMonth() + 1;
+  // 日
+  var d = now.get.Date();
+}
